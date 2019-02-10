c7d16102c3bd1ef7808aad1a3e7123c5c0adc077
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-02-10 14:19:41 -0500

    Create basic login
    - Per https://www.railstutorial.org/book/basic_login
    - Examples modified to fit our application
    - Next up: section 8.1.3

--- TourManagement/config/routes.rb
+++ TourManagement/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  # Auto-generated from scaffolding
   resources :visits
   resources :starts
   resources :locations
@@ -12,8 +13,16 @@ Rails.application.routes.draw do
   resources :tours
   resources :users
 
+  # Supporting login
+  # Per https://www.railstutorial.org/book/basic_login
+  # Route the incoming request (left) to controller action (right)
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'users#index'
+  # Home page
+  root 'sessions#new'
 
 end
