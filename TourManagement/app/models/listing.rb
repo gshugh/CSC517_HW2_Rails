37d4e772d44ff105b597c1e5dfb0ca99824ce7d0
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-02-10 18:38:34 -0500

    Display the responsible agent for each tour
    - Still don't actually have a link to the agent when we create a new tour
    - That's the next step

--- TourManagement/app/models/listing.rb
+++ TourManagement/app/models/listing.rb
@@ -1,4 +1,21 @@
 class Listing < ApplicationRecord
+
+  # Establish relationships
   belongs_to :user
   belongs_to :tour
+
+  # Method to get the name of the agent who created the given tour
+  def self.get_agent_name_for_tour(tour)
+    matching_listing = Listing.find_by(tour_id: tour.id)
+    if matching_listing
+      matching_user_id = matching_listing.read_attribute("user_id")
+      matching_user = User.find(matching_user_id)
+      matching_user_name = matching_user.read_attribute("name")
+      return matching_user_name
+    else
+      return nil
+    end
+
+  end
+
 end
