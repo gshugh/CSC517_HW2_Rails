c5405ea1e0db65e878129df48dbbb52f2c1cae53
George S. Hugh <hughgs@duke.edu>
2019-02-11 14:50:35 -0500

    Restrict number of seats to non-negative for booking model. Add comments.

--- TourManagement/app/models/booking.rb
+++ TourManagement/app/models/booking.rb
@@ -1,8 +1,16 @@
+################################################################################
+# A booking is the number of seats from a particular tour that is reserved by a
+# user.
+#
+# The number of seats must be a non-negative integer.
+
 class Booking < ApplicationRecord
 
   belongs_to :user
   belongs_to :tour
 
-  validates :num_seats, presence: true
+  validates :num_seats, presence: true,
+                        numericality: {only_integer: true,
+                                       greater_than_or_equal_to: 0}
 
 end
