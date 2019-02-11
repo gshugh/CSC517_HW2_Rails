0b602d7f9dc5a449f3a0bd572da265be2eec9ac8
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-02-11 12:19:59 -0500

    Add ability to review a tour
    - New review automatically associated with current user
    - Tour selected from drop-down
    - Reviews displayed with user and tour names rather than IDs for readability
    - Did not yet restrict ability to create reviews (only customers, only tours they've already taken)
    - Did not yet restrict ability to edit / delete reviews (only those the currently logged in user created)

--- TourManagement/app/models/review.rb
+++ TourManagement/app/models/review.rb
@@ -1,9 +1,21 @@
 class Review < ApplicationRecord
 
+  # Relationships
   belongs_to :user
   belongs_to :tour
 
+  # Validations
   validates :subject, presence: true
   validates :content, presence: true
+
+  # Get user name from review
+  def get_user_name
+    User.find(user_id).read_attribute("name")
+  end
+
+  # Get tour name from review
+  def get_tour_name
+    Tour.find(tour_id).read_attribute("name")
+  end
 
 end
