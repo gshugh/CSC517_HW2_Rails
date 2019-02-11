8c6c94bd7ddd914327a75cbf8e9e7d473e4232bc
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-02-11 13:06:46 -0500

    Add user friendly location drop-down to tour creation form

--- TourManagement/app/models/location.rb
+++ TourManagement/app/models/location.rb
@@ -1,12 +1,18 @@
 class Location < ApplicationRecord
 
+  # Relationships
   has_many :start_ats, dependent: :destroy
   has_many :visits, dependent: :destroy
-
   has_many :tours, through: :start_ats, dependent: :destroy
   has_many :tours, through: :visits, dependent: :destroy
 
+  # Validations
   validates :country, presence: true
   validates :state, presence: true
+
+  # Method to get a user-friendly string describing the location
+  def user_friendly_description
+    return state + ", " + country
+  end
 
 end
