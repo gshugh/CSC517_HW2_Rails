7a834df8e8376a5e9d7c218ac708ce629143773e
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-02-14 09:55:26 -0500

    Retain user's filtering choice after filtering applied

--- TourManagement/app/helpers/tours_helper.rb
+++ TourManagement/app/helpers/tours_helper.rb
@@ -1,2 +1,11 @@
 module ToursHelper
+
+  # Method to determine if the given location is one selected by the user in search filtering
+  def user_selected_location?(location)
+    if flash[:filters].length.positive? && flash[:filters]["desired_location"]
+      return flash[:filters]["desired_location"].to_i == location.id
+    else
+      return false
+    end
+  end
 end
