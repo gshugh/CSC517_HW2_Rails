9973b852e61ee36e96ffde8a86b0282bc0a32c3f
George S. Hugh <hughgs@duke.edu>
2019-02-17 04:38:24 -0500

    Copy controller tests to controller specs.

--- TourManagement/spec/controllers/bookings_controller_spec.rb
+++ TourManagement/spec/controllers/bookings_controller_spec.rb
@@ -2,4 +2,68 @@ require 'rails_helper'
 
 RSpec.describe BookingsController, type: :controller do
 
+  setup do
+
+    @booking = bookings(:one)
+    @tour = tours(:one)
+    @user = users(:one)
+
+    # Log in a user (gotta have a logged-in user to associate with any new bookings)
+    # But cannot use session helper from outside of the controller
+    # https://stackoverflow.com/questions/39465941/how-to-use-session-in-the-test-controller-in-rails-5?rq=1
+    # So, this is kind of a hack to fake a logged-in user
+    # Mimics params sent when logging in manually on the development application
+    post login_path, params: { session: { email: @user.email, password: @user.password } }
+
+  end
+
+  test "should get index" do
+    get bookings_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    # The bookings new view expects to be passed the tour we're currently working with,
+    #   so that the user doesn't have to select a tour
+    get new_booking_url(tour_id: @tour.id)
+    assert_response :success
+  end
+
+  test "should create booking" do
+    assert_difference('Booking.count') do
+      post bookings_url, params: { booking: {
+          num_seats: @booking.num_seats,
+          tour_id: @booking.tour_id,
+          user_id: @booking.user_id,
+          # Add a booking strategy (1 - Book All Seats)
+          strategy: 1
+      } }
+    end
+
+    assert_redirected_to booking_url(Booking.last)
+  end
+
+  test "should show booking" do
+    get booking_url(@booking)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_booking_url(@booking)
+    assert_response :success
+  end
+
+  test "should update booking" do
+    patch booking_url(@booking), params: { booking: { num_seats: @booking.num_seats, tour_id: @booking.tour_id, user_id: @booking.user_id } }
+    assert_redirected_to booking_url(@booking)
+  end
+
+  test "should destroy booking" do
+    assert_difference('Booking.count', -1) do
+      delete booking_url(@booking)
+    end
+
+    assert_redirected_to bookings_url
+  end
+
 end
