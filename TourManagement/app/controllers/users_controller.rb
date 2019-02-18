fe02a71aa6b5eebcb18b55bcea27d348418de2bb
Aurora Tiffany-Davis <aurora.tdavis@gmail.com>
2019-02-18 13:34:26 -0500

    Differentiate new user creation (sign up vs. admin action)

--- TourManagement/app/controllers/users_controller.rb
+++ TourManagement/app/controllers/users_controller.rb
@@ -29,7 +29,13 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        if logged_in?
+          # If you created a new user when already logged in, you're an admin
+          format.html { redirect_to @user, notice: 'User was successfully created.' }
+        else
+          # If you created a new user when not logged in, you must be signing up
+          format.html { redirect_to login_path, notice: 'User was successfully created.  Please Log In.' }
+        end
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
